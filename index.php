e31a284e9a0daadc1f1bab7ee817de352262d051
Tabil-G-BPC-Cyan <32195045+TabilGwyneth@users.noreply.github.com>
2017-11-10 10:14:57 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE HTML>
+<html>
+<body>
+
+<?php
+
+$x = rand(1,4);
+
+echo $x;
+echo "<br>";
+
+switch ($x)  {
+     case "1":
+         echo "I rolled a one!";
+           break;
+       case "2":
+         echo "I rolled a two!";
+           break;
+       case "3":
+         echo "I a rolled a three";
+           break;
+       default:
+         echo "You neither rolled 1, 2, nor 3!";
+ }
+
+  
+?>
+  
+</body>
+</html>
